9c06d79853cb312b2047785c3ce2dde9d86d1b16
satish <satishkonduru0225@gmail.com>
2025-09-20 20:49:51 +0530

    Static friends component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 const initialFriends = [
   {
     id: 118836,
-    name: "RSK",
-    image: "./../public/images/RSK.jpeg",
+    name: "Micky",
+    image: "/images/Micky.jpg",
     balance: -7,
   },
   {
     id: 933372,
     name: "Renu",
-    image: "./../public/images/Renu.jpg",
+    image: "/images/Renu.jpg",
     balance: 20,
   },
   {
     id: 499476,
     name: "Chikkie",
-    image: "./../public/images/Chikkie.jpg",
+    image: "/images/Chikkie.jpg",
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You have to give ₹ {Math.abs(friend.balance)} to {friend.name}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} has to give ₹ {friend.balance} to you.
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
